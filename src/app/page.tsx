18e6e840a9797e107c875011f58fa21a5267e5c4
awera <aw3ra@proton.me>
2023-11-19 15:51:48 +1100

    final touches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,14 @@ import StepFooter from './components/stepFooter'
 import StepHeader from './components/stepHeader'
 import StepStaking from './components/stepStaking'
 import { footerItems } from './data'
+import Head from 'next/head';
 import Toast from './components/sub-components/toast'
-
 import React, { FC, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
     WalletModalProvider,
 } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 
 require('@solana/wallet-adapter-react-ui/styles.css');
 
@@ -25,15 +23,20 @@ export default function Home() {
   const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], []);
   return (
     <ConnectionProvider endpoint={rpcUrl}>
-    <WalletProvider wallets={wallets} autoConnect>
-    <Toast />
+      <WalletProvider wallets={wallets} autoConnect>
+        <Toast />
         <WalletModalProvider>
-              <main className="flex min-h-screen flex-col items-center justify-top gap-24 py-8 bg-black text-white">
+          <Head>
+            <title>Head test</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="icon" href="/stepCoin.png" />
+          </Head>
+          <main className="flex min-h-screen flex-col items-center justify-top gap-8 py-8 bg-black text-white">
 
-                <StepHeader company="Step" logo="/stepLogo.svg" description="Wallet Connect" />
-                <StepStaking company="Step" logo="/stake.svg" description="Wallet Connect" />
-                <StepFooter items={footerItems} />
-              </main>
+            <StepHeader company="Step" logo="/stepLogo.svg" description="Wallet Connect" />
+            <StepStaking company="Step" logo="/stake.svg" description="Wallet Connect" />
+            <StepFooter items={footerItems} />
+          </main>
         </WalletModalProvider>
       </WalletProvider>
   </ConnectionProvider>
